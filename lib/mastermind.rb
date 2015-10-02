344d06047f8f9d2762d5d8c33ca30a1c95e5b82f
AdeyBee <mradeybee@gmail.com>
2015-10-02 10:15:19 +0100

    Game initializing code added

--- lib/mastermind.rb
+++ lib/mastermind.rb
@@ -1,5 +1,13 @@
 require "mastermind/version"
+require "colorize"
+require_relative "main"
+require_relative "player"
+require_relative "computer"
+require_relative "game_engine"
 
 module Mastermind
-  # Your code goes here...
+p Code_generator::Computer
+Starter.new.welcome
+Starter.new.ask
+Game_Engine.new.game
 end
